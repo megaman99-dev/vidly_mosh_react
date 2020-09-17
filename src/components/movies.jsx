4cd039d88db6a3a5411a18313eebf89b78904d6b
Abdul Raqueeb <raqueebabdul@RAQUEEBs-Air.local>
2020-09-17 17:24:56 +0530

    Finished

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,8 +5,48 @@ class Movies extends Component {
     state = { 
         movies : getMovies()
      };
+
+     handleDelete = (movie) => {
+         const movies = this.state.movies.filter(m => m._id !== movie._id);
+         this.setState({movies});
+     };
+     
     render() { 
-        return (  );
+        const { length : count } = this.state.movies;
+        if ( count === 0) return <p>There are no movies in the database.</p>
+        
+        return ( 
+        <div className="container">
+        <div className = "row">
+        <p>Showing {count} movies in database.</p>
+        <table className = "table ">
+            <thead>
+                <tr>
+                    <th scope = 'col'>Title</th>
+                    <th scope = 'col'>Genre</th>
+                    <th scope = 'col'>Stock</th>
+                    <th scope = 'col'>Rate</th>
+                    <th scope = "col"></th>
+                </tr>   
+            </thead>
+            <tbody>
+                {this.state.movies.map(obj => 
+                <tr key = {obj._id} >
+                    <th scope = "col">{obj.title}</th>
+                    <td>{obj.genre.name}</td>
+                    <td>{obj.numberInStock}</td>
+                    <td>{obj.dailyRentalRate}</td>
+                    <td>
+                        <button onClick={( ) => this.handleDelete(obj) } className = "btn btn-danger">Delete</button>
+                    </td>
+                </tr>
+                
+                ) }
+            </tbody>
+        </table>
+        </div>
+        </div>
+            );
     }
 }
  
